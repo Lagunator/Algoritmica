376a94290791351484ce458ff6e432b7dbf8bd58
Lagunator <102009436+Lagunator@users.noreply.github.com>
2022-10-22 18:30:09 -0400

    Update CodigoPrim.cpp

--- Teoria de grafos/Prim/CodigoPrim.cpp
+++ Teoria de grafos/Prim/CodigoPrim.cpp
@@ -1,80 +1,46 @@
-//cola de prioridad
 #include <bits/stdc++.h>
 #define input freopen("in.txt", "r", stdin)
+#define output freopen("out.txt", "w", stdout)
 using namespace std;
+vector<pair<int, int> > grafo[100000];
+bool visitados[100000];
 
-vector<pair<int, int>> grafo[100000];
-vector<pair<int,int>> mst[100000];
-set <int> nodos[100000];
-multiset<pair<int,int>> colaPrioridad;
-int visitados[100000];
-int parent[100000];
-int parentWeight[100000];
-int aristas;
-int numNodos;
 
-void findMST(int nodoInicial){
-    colaPrioridad.insert(make_pair(0, nodoInicial));
-    memset(visitados, false, sizeof(visitados));
+int Prim(int verticeInicial){
+    multiset<pair<int, int>> colaPrioridad;
+    colaPrioridad.insert(make_pair(0, verticeInicial)); 
     while (!colaPrioridad.empty()){
-        pair<int, int> nodoActual = *colaPrioridad.begin();
-        colaPrioridad.erase(colaPrioridad.begin());
-        int verticeActual = nodoActual.second;
-        if (!visitados[verticeActual]){
-            visitados[verticeActual] = true;
-            for (int i = 0; i < grafo[verticeActual].size(); i++){
-                int verticeVecino = grafo[verticeActual][i].first;
-                int pesoVecino = grafo[verticeActual][i].second;
-                if(!visitados[verticeVecino]){
-                    if(parent[verticeVecino]==-1 || (pesoVecino<parentWeight[verticeVecino]
-                    && parentWeight[verticeVecino]!=-1)){
-                        parent[verticeVecino]=verticeActual;
-                        parentWeight[verticeVecino]=pesoVecino;
-                    }
-                    colaPrioridad.insert(make_pair(pesoVecino, verticeVecino));
-                }
-            }
-            if(parent[verticeActual]!=-1 && parentWeight[verticeActual]!=-1 ){
-                mst[verticeActual].push_back(
-                            make_pair(parent[verticeActual],parentWeight[verticeActual]));
-                mst[parent[verticeActual]].push_back(
-                            make_pair(verticeActual,parentWeight[verticeActual]));
-            }
-            
+        pair<int, int> verticeActual = *colaPrioridad.begin();
+        colaPrioridad.erase(colaPrioridad.begin()); 
 
-        }
+        int vertice = verticeActual.second; 
+        int peso = verticeActual.first; 
+
+        if (!visitados[vertice]){ 
+            visitados[vertice] = true;
+            answer += peso;
+            for (int i = 0; i < grafo[vertice].size(); ++i){ 
+                int verticeVecino = grafo[vertice][i].second;
+                int pesoVecino = grafo[vertice][i].first;
+                colaPrioridad.insert(make_pair(pesoVecino, verticeVecino)); 
+            }
+        }   
     }
+
+    return answer; 
 }
 
-void printMst(){
-	cout << "Destino\tOrigen\tPeso\n";
-	for(int i = 0; i < numNodos; i++){
-        vector<pair<int,int>> nodoActual = mst[i];
-        for (auto it: nodoActual){
-            cout << i+1 << "\t" << it.first + 1<< "\t" << it.second << "\n";
-        }
-	}
-}
-
-int main(){
-    input;
-    cin>>numNodos>>aristas;
-    
+int main()
+{
+    int vertices, aristas;
+    cin >> vertices >> aristas;
     for (int i = 0; i < aristas; i++)
     {
-        int a,b,c;
-        cin>>a>>b>>c;
-        a--;
-        b--;
-        grafo[a].push_back(make_pair(b,c));
-        grafo[b].push_back(make_pair(a,c));
+        int ini,fin,peso; 
+        cin>>ini>>fin>>peso;
+        grafo[ini].push_back(make_pair(peso,fin));
+        grafo[fin].push_back(make_pair(peso,ini));
     }
-    memset(parent, -1, sizeof(parent));
-    memset(parent, -1, sizeof(parentWeight));
-    int nodoInicial;
-    cin>>nodoInicial;
-    nodoInicial--;
-    findMST(nodoInicial);
-    printMst();
+    cout <<  Prim(0) << endl;
     return 0;
 }
